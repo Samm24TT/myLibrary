f2c50d4e22ad8271e8f73555cdd26f1ad0921851
Sam24 <aclian4321@gmail.com>
2026-05-16 04:21:27 +0800

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div className="bg-gradient-to-r from-indigo-300 to-purple-400 h-48 flex justify-center items-center">
+      Welcome Home
+    </div>
+  );
+}
+
+export default Home;
