601e9c96687b707a780908f8d22cf70e295887b1
Sam24 <aclian4321@gmail.com>
2026-05-17 05:29:51 +0800

    add animation transition between react page

--- src/pages/layouts/Layout.jsx
+++ src/pages/layouts/Layout.jsx
@@ -1,14 +1,29 @@
 // import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "../../components/Navbar";
+import { SwitchTransition, CSSTransition } from "react-transition-group";
+import { useRef } from "react";
+import "./styles.css";
 
 export default function Layout() {
+  const location = useLocation();
+  console.log(location.pathname);
+  const nodeRef = useRef(null);
   return (
     <div>
       <Navbar />
-      <div className="max-w-6xl mx-auto p-3">
-        <Outlet />
-      </div>
+      <SwitchTransition>
+        <CSSTransition
+          timeout={200}
+          classNames="fade"
+          key={location.pathname}
+          nodeRef={nodeRef}
+        >
+          <div className="max-w-6xl mx-auto p-3" ref={nodeRef}>
+            <Outlet />
+          </div>
+        </CSSTransition>
+      </SwitchTransition>
     </div>
   );
 }
